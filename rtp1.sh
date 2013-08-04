7d9dc4d2880ae6d1ae37d6641c2de50efa24a540
Oscar Brito <oscar@Oscars-MacBook-Air.local>
2013-08-04 19:24:53 +0100

    removed -R from rtp1.sh

--- rtp1.sh
+++ rtp1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rtmpdump.exe -v -r rtmp://h2j2.rtp.pt/livetv -W http://www.rtp.pt/play/player.swf -p http://www.rtp.pt/services/rtpplay/pub/config_page/rtp.pt/play/direto/tv/rtp1 --playpath "2ch5h264" -R --live -b 20000 -V | vlc.exe --file-caching=20000 -
+rtmpdump -v -r rtmp://h2j2.rtp.pt/livetv -W http://www.rtp.pt/play/player.swf -p http://www.rtp.pt/services/rtpplay/pub/config_page/rtp.pt/play/direto/tv/rtp1 --playpath "2ch5h264" --live -b 20000 -V | vlc --file-caching=20000 -
